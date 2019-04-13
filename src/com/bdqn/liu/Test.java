b294560518023a8d2de3f9b6d7dded037ff2ac4a
liu1064028419 <1064028419@qq.com>
2019-04-13 11:42:07 +0800

    first commit

---
+++ src/com/bdqn/liu/Test.java
@@ -0,0 +1,10 @@
+package com.bdqn.liu;
+
+/**
+ * Created by fate on 2019/4/13.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("hi world!");
+    }
+}
